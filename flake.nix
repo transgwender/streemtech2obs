5d8b1dbc9e0801dba6d060a809e9cb88efe123bf
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-07-15 16:34:38 -0700

    Initial

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    # Use a github flake URL for real packages
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.12";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs; # pass through all inputs and bring them into scope
+
+    # Build the output set for each default system and map system sets into
+    # attributes, resulting in paths such as:
+    # nix build .#packages.x86_64-linux.<name>
+    flake-utils.lib.eachDefaultSystem (system:
+
+      # let-in expressions, very similar to Rust's let bindings.  These names
+      # are used to express the output but not themselves paths in the output.
+      let
+
+        # create nixpkgs that contains rustBuilder from cargo2nix overlay
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+        };
+
+        # create the workspace & dependencies package set
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        # this is the output (recursive) set (expressed for each system)
+
+        # the packages in `nix build .#packages.<system>.<name>`
+        packages = {
+          # nix build .#streemtech2obs
+          # nix build .#packages.x86_64-linux.streemtech2obs
+          streemtech2obs = (rustPkgs.workspace.streemtech2obs {});
+          # nix build
+          default = packages.streemtech2obs; # rec
+        };
+      }
+    );
+}
