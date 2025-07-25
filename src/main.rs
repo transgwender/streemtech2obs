8b221576a1d505041e2fa6035d9b14e7da5b16c2
Gwendolyn Kornak <gwen@gwenkornak.ca>
2025-07-24 17:08:02 -0700

    Cleanup errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{fs, process};
+use crate::models::Config;
 use std::env;
 use std::net::IpAddr;
-use crate::models::Config;
+use std::{fs, process};
 
 mod models;
 mod handlers;
@@ -13,27 +13,26 @@ async fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let file_path = parse_config(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
+    let config: Config = parse_config(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing configuration: {err}");
         process::exit(1);
     });
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-    let config: Config = serde_json::from_str(&contents).unwrap();
 
     println!("Server starting at http://{}:{}", config.ip, config.port);
     let addr = config.ip.parse::<IpAddr>().unwrap();
     warp::serve(routes).run((addr, config.port)).await;
 }
 
-fn parse_config(args: &[String]) -> Result<&str, &'static str> {
+
+fn parse_config(args: &[String]) -> Result<Config, &str> {
     if args.len() < 2 {
         return Err("not enough arguments");
     }
 
     let file_path = &args[1];
 
-    Ok(file_path)
+    let contents = fs::read_to_string(file_path).map_err(|_| "cannot read file")?;
+    let config: Config = serde_json::from_str(&contents).map_err(|_| "cannot deserialize")?;
+
+    Ok(config)
 }
